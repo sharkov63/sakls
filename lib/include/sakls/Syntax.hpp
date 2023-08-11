0f91a167f4b96e79c6298daa8d610ddf5b081ae9
Danila Usachev <duesna897@gmail.com>
2023-08-11 23:17:09 +0300

    [#50] Use std::span for SyntaxStackRef

--- lib/include/sakls/Syntax.hpp
+++ lib/include/sakls/Syntax.hpp
@@ -9,6 +9,7 @@
 
 #include <initializer_list>
 #include <iterator>
+#include <span>
 
 namespace sakls {
 
@@ -35,56 +36,24 @@ public:
 /// a convenience STL-like container wrapper around sakls_SyntaxStackRef.
 ///
 /// This type is small and shall be passed by value, not by reference.
-class SyntaxStackRef {
+class SyntaxStackRef : public std::span<const SyntaxNode> {
 public:
-  using value_type = SyntaxNode;
-  using pointer = value_type *;
-  using const_pointer = const value_type *;
-  using reference = value_type &;
-  using const_reference = const value_type &;
-  using iterator = const_pointer;
-  using const_iterator = const_pointer;
-  using reverse_iterator = std::reverse_iterator<iterator>;
-  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-  using size_type = size_t;
-  using difference_type = ptrdiff_t;
-
-private:
-  const SyntaxNode *data_;
-
-  size_t size_;
-
-public:
-  SyntaxStackRef() : data_(nullptr), size_(0) {}
-
-  SyntaxStackRef(const SyntaxNode *data, size_t size)
-      : data_(data), size_(size) {}
+  using std::span<const SyntaxNode>::span;
 
   SyntaxStackRef(sakls_SyntaxStackRef cRef)
-      : data_(reinterpret_cast<SyntaxNode *>(cRef.data)), size_(cRef.size) {}
+      : std::span<const SyntaxNode>(
+            reinterpret_cast<const SyntaxNode *>(cRef.data), cRef.size) {}
 
 #if defined(__GNUC__) && (__GNUC__ >= 9)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Winit-list-lifetime"
 #endif
   SyntaxStackRef(const std::initializer_list<SyntaxNode> &list)
-      : data_(list.begin() != list.end() ? list.begin() : nullptr),
-        size_(list.size()) {}
+      : SyntaxStackRef(list.begin() != list.end() ? list.begin() : nullptr,
+                       list.size()) {}
 #if defined(__GNUC__) && (__GNUC__ >= 9)
 #pragma GCC diagnostic pop
 #endif
-
-  iterator begin() const { return data_; }
-  iterator end() const { return data_ + size_; }
-
-  reverse_iterator rbegin() const { return reverse_iterator(end()); }
-  reverse_iterator rend() const { return reverse_iterator(begin()); }
-
-  bool empty() const { return size_ == 0; }
-
-  const SyntaxNode *data() { return data_; }
-
-  size_t size() const { return size_; }
 };
 
 } // namespace sakls
