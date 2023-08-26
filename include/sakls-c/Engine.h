f49e92a1ca07ce181ef964bbcd42d78c839b7f1c
Danila Usachev <duesna897@gmail.com>
2023-08-26 12:18:54 +0300

    [#64] Refactored and extended Engine API (#65)

--- include/sakls-c/Engine.h
+++ include/sakls-c/Engine.h
@@ -2,19 +2,59 @@
 /// \file Engine.h
 /// SAKLS Engine C API.
 ///
+/// Delegates to Engine C++ API.
+///
 #ifndef SAKLS_C_ENGINE_H
 #define SAKLS_C_ENGINE_H
 
 #include "sakls-c/ExternC.h"
+#include "sakls-c/Syntax.h"
 
 SAKLS_EXTERN_C_BEGIN
 
 /// \deprecated Temporary, while schema is not supported.
 void *sakls_Engine_createWithDefaultSchema(void *layoutBackend);
 
+/// \brief Set the Engine state to inactive.
+///
+/// \param engine Pointer to the SAKLS engine.
+/// \return 0 on success; non-zero value on error.
+int sakls_Engine_reset(void *engine);
+
+/// \brief Set currently observed syntax node.
+///
+/// \pre newNode must not be an ignored syntax node.
+///
+/// This is a cheap way to update the Engine; it allows to directly set the
+/// current syntax node without passing the whole syntax stack. However, the
+/// user must guarantee that the new node is not an ignored syntax node.
+///
+/// \param engine Pointer to the SAKLS engine.
+/// \param newNode New observed syntax node.
+/// \param force Change the layout even if the type of the current syntax node
+/// hasn't been changed.
+/// \return 0 on success; non-zero value on error.
+int sakls_Engine_setNewSyntaxNode(void *engine, struct sakls_SyntaxNode newNode,
+                                  int force);
+
+/// \brief Replace the currently observed syntax stack with a new one.
+///
+/// This is the most flexible way to update an Engine, but it is also the
+/// most expensive one: in the worst case it requires to traverse the whole
+/// syntax stack.
+///
+/// \param engine Pointer to the SAKLS engine.
+/// \param synStack The new syntax stack.
+/// \param force Change the layout even if the type of the current syntax node
+/// hasn't been changed.
+/// \return 0 on success; non-zero value on error.
+int sakls_Engine_setNewSyntaxStack(void *engine,
+                                   struct sakls_SyntaxStackRef synStack,
+                                   int force);
+
 /// \brief Destroy a SAKLS engine.
 ///
-/// \param engine Pointer to SAKLS engine to destory.
+/// \param engine Pointer to SAKLS engine to destroy.
 void sakls_Engine_delete(void *engine);
 
 SAKLS_EXTERN_C_END
--- include/sakls/Engine.hpp
+++ include/sakls/Engine.hpp
@@ -13,61 +13,105 @@
 
 namespace sakls {
 
-/// SAKLS engine: the key class that is actually responsible for
+/// \brief SAKLS engine: the key class that is actually responsible for
 /// making decisions on switching keyboard layout.
 ///
 /// When the contents or the cursor position of the currently edited file
-/// change, the editor (or a plugin) makes calls to the SAKLS engine,
-/// passing the syntax information as parameters. Then, the engine makes the
-/// decision and sets the chosen keyboard layout using the layout backend.
+/// change, the editor (or a plugin) makes calls to the Engine, passing syntax
+/// information as parameters. Then, the engine makes the decision and sets the
+/// chosen keyboard layout using the layout backend.
 ///
 /// The algorithm, which tells the engine what keyboard layout to set at the
 /// current moment, knowing syntax information, is described by Schema class.
 ///
 /// Note that Engine does not take ownership of layout backend and schema.
 class Engine {
-  ILayoutBackend &layoutBackend;
-  SchemaTranslator translator;
-
-  std::unordered_map<SyntaxNodeType, LayoutID> memorized;
-  std::unordered_map<SyntaxNodeType, LayoutID> forced;
-  std::unordered_set<SyntaxNodeType> ignored;
-
-  std::optional<SyntaxNode> current;
-
-  void configure(const Schema &schema);
-
-  SyntaxNode getRelevantTop(SyntaxStackRef synStack) const;
-
-  void saveCurrentMemorized();
-
-  void updateNewSyntaxNode(SyntaxNode newNode, bool force = false);
-
 public:
-  /// Create a SAKLS engine.
+  /// @name Engine C++ API
+  /// When modifying this, modify Engine C API as well.
+  /// @{
+
+  /// \brief Create a SAKLS engine.
   ///
-  /// The created SAKLS engine has uninitialized state:
-  /// it does not observe any syntax stack. To initialize it,
-  /// call an update method.
+  /// Created engine is inactive (see Engine state).
   ///
-  /// \param layoutBackend Reference to the layout backend.
-  /// \param schema High-level SAKLS schema.
-  /// \param translator Schema translator valid for the time being.
+  /// \param layoutBackend Layout backend for the engine to use.
+  /// \param schema High-level SAKLS schema, which configures the engine.
+  /// \param translator Schema translator, which translates string syntax node
+  /// types (seen in high-level schema) into integer syntax node types
+  /// (used in Engine for computations). In case when the user is also aware
+  /// about the translation, and passes integer syntax node types in calls to
+  /// Engine API, the user must guarantee that this translation is valid during
+  /// the whole lifetime of the Engine.
   Engine(ILayoutBackend &layoutBackend, const Schema &schema = Schema(),
          SchemaTranslator translator = SchemaTranslator()) noexcept;
 
-  /// Update the SAKLS engine: completely replace the currently observed syntax
-  /// stack with a new one.
+  /// \brief Set the Engine state to inactive.
+  void reset();
+
+  /// \brief Set currently observed syntax node.
   ///
-  /// This is the most flexible way to update a SAKLS engine, but it is also the
+  /// \pre newNode must not be an ignored syntax node.
+  ///
+  /// This is a cheap way to update the Engine; it allows to directly set the
+  /// current syntax node without passing the whole syntax stack. However, the
+  /// user must guarantee that the new node is not an ignored syntax node.
+  ///
+  /// \param newNode New observed syntax node.
+  /// \param force Change the layout even if the type of the current syntax node
+  /// hasn't been changed.
+  void setNewSyntaxNode(SyntaxNode newNode, bool force = false);
+
+  /// \brief Replace the currently observed syntax stack with a new one.
+  ///
+  /// This is the most flexible way to update an Engine, but it is also the
   /// most expensive one: in the worst case it requires to traverse the whole
   /// syntax stack.
   ///
   /// \param synStack The new syntax stack.
   /// \param force Change the layout even if the type of the current syntax node
   /// hasn't been changed.
-  void updateNewSyntaxStack(SyntaxStackRef synStack,
-                            bool force = false) noexcept;
+  void setNewSyntaxStack(SyntaxStackRef synStack, bool force = false);
+
+  /// @}
+
+protected:
+  /// The layout backend, which handles layout switching demanded by the Engine.
+  ILayoutBackend &layoutBackend;
+  /// \brief Translates string syntax node types into integers.
+  ///
+  /// If user introduces new string syntax node types through Engine API,
+  /// #translator will assign them new integers.
+  SchemaTranslator translator;
+
+  /// @name Engine State.
+  /// At any point of time, a SAKLS engine can be either active, or inactive.
+  /// When Engine is active, it observes a syntax node #node, which is (usually)
+  /// the bottom of the currently observed syntax stack. When Engine is
+  /// inactive, it does not observe any syntax node.
+  /// @{
+
+  /// \brief Currently observed syntax node.
+  ///
+  /// It is \p std::nullopt if and only if the Engine is inactive.
+  std::optional<SyntaxNode> node;
+  /// \brief Maps memorized syntax node types to their current layout.
+  ///
+  /// This mapping can be changed, after user manually switches
+  /// the layout while being on a memorized syntax node.
+  std::unordered_map<SyntaxNodeType, LayoutID> memorized;
+  /// Maps forced syntax node types to their layout.
+  std::unordered_map<SyntaxNodeType, LayoutID> forced;
+  /// The set of ignored syntax node types.
+  std::unordered_set<SyntaxNodeType> ignored;
+
+  /// @}
+
+  void keepUp();
+
+  void configure(const Schema &schema);
+
+  SyntaxNode getRelevantTop(SyntaxStackRef synStack) const;
 };
 
 } // namespace sakls
